2648641d1ffdf07878179c4a42ab00a9f38b55c4
Sebastian Krueger <krueger@secra.de>
2013-09-19 11:04:07 +0200

    First walking demo but shows not the effect right now

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ demo-responsive-viewport
 ========================
 
 Shows the impact in the use of metatag viewport
+
+For use in iPad (viewport-width: 980px / 1024px)
---
+++ viewport.php
@@ -0,0 +1,22 @@
+﻿<!doctype html>
+<head>
+	<meta charset="utf-8">
+	<?php
+	    if(isset($_GET["viewport"])) {
+	?>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<?php 
+            $title="with viewport meta!";
+	    } else {
+	        $title="without viewport meta!";
+	    }
+	?>
+	<title>Test <?php echo $title; ?></title>
+    <link rel="stylesheet" type="text/css" href="viewport.css">
+</head>
+<body>
+	<div id="container">
+		<div id="bluebox"></div>
+	</div>
+</body>
+</html>
